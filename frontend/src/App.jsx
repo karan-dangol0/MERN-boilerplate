a0a196184c85885c91689b8aaf82958d52436f00
Karan Dangol <150545082+high-pie-pepper@users.noreply.github.com>
2025-09-15 13:03:37 +0545

    added all folders

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/App.css";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-red-500 text-3xl text-center absolute top-2/4 left-2/4">Hey, its working😁</h1>
+    </>
+  );
+};
+
+export default App;
